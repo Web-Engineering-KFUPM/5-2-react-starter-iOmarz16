fab2e758a71eb1d76f68434c46e3c2e0fb20d680
iOmarz16 <amory1424h@gmail.com>
2025-10-05 14:38:55 +0300

    5.2  Omar Alharbi

---
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function StudentCard(props) {
+
+  return (
+    <div className="student-card">
+      <h3>Name: {props.name}</h3>
+      <p>Student ID: {props.id}</p>
+      <p>Department: {props.dept}</p>
+    </div>
+  );
+}
+export default StudentCard;
